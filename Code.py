597fc6da8f3dc84194bf7e36c37de177451d3a61
Ayush Pai <ayushpai@ayushpai.com>
2026-05-08 14:46:10 +0530

    update code.py

--- Code.py
+++ Code.py
@@ -3,7 +3,7 @@ from tkinter import messagebox, simpledialog, Tk
 root = Tk()
 root.withdraw()
 
-the_world = {}
+passwords = {}
 
 def read_from_file():  
         with open("passwords.txt") as file:
@@ -11,7 +11,5 @@ def read_from_file():
                 line = line.strip()
                 if not line:
                     continue
-                country, city = line.split("/")
-                country = country.lower()
-                city = city.lower()
-                the_world[country] = city
+                app, password = line.split("/")
+                passwords[app] = password
